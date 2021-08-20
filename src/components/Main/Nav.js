202b8259cbdb84d856313a849ed77377b4250595
hollylouisarose <hlrstratton@gmail.com>
2021-08-20 15:06:26 +0100

    initial navbar styles

--- src/components/Main/Nav.js
+++ src/components/Main/Nav.js
@@ -1,9 +1,29 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
 function Nav() {
 
   return (
-    <>
-      <h1>Nav:</h1>
-    </>
+    <nav className="top-nav">
+      <div className="logo">
+        <Link to="/">
+          <h3>PictureREST</h3>
+        </Link>
+      </div>
+
+      <div className="nav-links">
+        <Link to="/login">
+          Login
+        </Link>
+        <Link to="/register">
+          Sign up
+        </Link>
+        <Link to="/images/new" className="red-button-small">
+          Upload a photo
+        </Link>
+      </div>
+
+    </nav>
   )
 }
 

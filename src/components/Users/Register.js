16e71f0ca8973685f39c5e2523fb5785a6beee1e
hollylouisarose <hlrstratton@gmail.com>
2021-08-20 16:47:53 +0100

    adjusted text styles

--- src/components/Users/Register.js
+++ src/components/Users/Register.js
@@ -4,8 +4,8 @@ function Register() {
     <section className="section">
       <div className="center-container">
         <div>
-          <h2>Welcome to pictureREST</h2>
-          <p>Sign up to collect, share and find great photo spots</p>
+          <h2 className="text-is-centered">Welcome to pictureREST</h2>
+          <p className="text-is-centered">Sign up to collect, share and find great photo spots</p>
         </div>
         <div className="form-container">
           <form>

80ecf328d9c6d955a00b5d4c82e4a350a3d63187
hollylouisarose <hlrstratton@gmail.com>
2021-08-24 09:46:13 +0100

    inital homepage filter

---
+++ src/functionLib/variables.js
@@ -0,0 +1,13 @@
+// * for global variables 
+
+export const selectOptions = [
+  { value: 'Beach', label: 'Beach' },
+  { value: 'Mountains', label: 'Mountains' },
+  { value: 'Ocean', label: 'Ocean' },
+  { value: 'Lake', label: 'Lake' },
+  { value: 'Forest', label: 'Forest' },
+  { value: 'Desert', label: 'Desert' },
+  { value: 'Meadow', label: 'Meadow' },
+  { value: 'Valley', label: 'Valley' }
+]
+

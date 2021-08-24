dc948684854c542bd57add88690c97c6754a607b
hollylouisarose <hlrstratton@gmail.com>
2021-08-24 17:42:43 +0100

    errors added register form

--- src/components/Users/Register.js
+++ src/components/Users/Register.js
@@ -1,28 +1,36 @@
 import React from 'react'
+import { createUser } from '../../functionLib/api'
+
+const initialState = {
+  username: '',
+  email: '',
+  password: '',
+  passwordConfirmation: '',
+}
 
 function Register() {
-
-  const [formData, setFormData] = React.useState({
-    username: '',
-    email: '',
-    password: '',
-    passwordConfirmation: '',
-  })
+  const [formData, setFormData] = React.useState(initialState)
+  const [formErrors, setFormErrors] = React.useState(initialState)
 
   const handleChange = (e) => {
     setFormData({ ...formData, [e.target.name]: e.target.value })
+    setFormErrors({ ...formErrors, [e.target.name]: '' })
   }
 
-  console.log(formData)
-
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault()
     try {
-      console.log('submit')
-    } catch (error) {
-      console.log(error)
+      await createUser(formData)
+    } catch (err) {
+      err.response.data.username = 'Oops! Username required'
+      err.response.data.email = 'Oops! Email required'
+      err.response.data.password = 'Oops! Password required'
+      err.response.data.passwordConfirmation = 'Oops! Passwords must match'
+      setFormErrors(err.response.data)
     }
   }
+
+  
 
   return (
     <section className="section">
@@ -36,46 +44,54 @@ function Register() {
             <div className="field">
               <label className="label">Username</label>
               <div>
+                {formErrors.username && (<p className="error-text">{formErrors.username}</p>)}
                 <input
                   onChange={handleChange}
                   className="input"
                   placeholder="Username"
-                  name="userName"
+                  name="username"
+                  value={formData.username}
                 />
               </div>
             </div>
             <div className="field">
               <label className="label">Email</label>
               <div>
+                {formErrors.email && (<p className="error-text">{formErrors.email}</p>)}
                 <input
                   onChange={handleChange}
                   className="input"
                   placeholder="Email Address"
                   name="email"
+                  value={formData.email}
                 />
               </div>
             </div>
             <div className="field">
               <label className="label">Password</label>
               <div className="control">
+                {formErrors.password && (<p className="error-text">{formErrors.password}</p>)}
                 <input
                   onChange={handleChange}
                   type="password"
                   className="input"
                   placeholder="Password"
                   name="password"
+                  value={formData.password}
                 />
               </div>
             </div>
             <div className="field">
               <label className="label">Password Confirmation</label>
               <div className="control">
+                {formErrors.passwordConfirmation && (<p className="error-text">{formErrors.passwordConfirmation}</p>)}
                 <input
                   onChange={handleChange}
                   type="password"
                   className="input"
-                  placeholder="Password"
-                  name="password"
+                  placeholder="Confirm your password"
+                  name="passwordConfirmation"
+                  value={formData.passwordConfirmation}
                 />
               </div>
             </div>

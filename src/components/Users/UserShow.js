9dd271a3550e478dcc7feae13ff3733caff80919
felixfe <felixfennevans@gmail.com>
2021-08-26 17:07:17 +0100

    follow

--- src/components/Users/UserShow.js
+++ src/components/Users/UserShow.js
@@ -1,9 +1,10 @@
 
 import React from 'react'
-import { useParams, Link } from 'react-router-dom'
+import { useParams, Link, useLocation } from 'react-router-dom'
 import { showUser, getImages } from '../../functionLib/api'
 import { isAuthenticated, isOwner } from '../../functionLib/auth'
-
+import { editUser } from '../../functionLib/api'
+import { getPayload } from '../../functionLib/auth'
 
 function UserShow() {
 
@@ -12,6 +13,10 @@ function UserShow() {
   const [imageData, setImageData] = React.useState()
   const [filteredData, setFilteredData] = React.useState()
   const [owner, setOwner] = React.useState()
+  const [following, setFollowing] = React.useState()
+  const [canFollow, setCanFollow] = React.useState()
+  const currentUser = getPayload().sub
+  const location = useLocation()
   
 
   console.log(userId)
@@ -64,6 +69,79 @@ function UserShow() {
 
 
 
+  // follow button
+
+  React.useEffect(() => {
+    setCanFollow(userId !== currentUser)
+  }, [location])
+
+  React.useEffect(() => {
+    async function compareUser(){
+      try {
+        const user = await showUser(currentUser)
+        const userData = user.data
+        const userToEdit = { ...userData }
+        console.log(userToEdit.myFollowing)
+        // console.log(inputs.addedBy)
+        if (userToEdit.myFollowing.includes(`${userId}`)){
+          setFollowing(true)
+        } else {
+          setFollowing(false)
+        }
+      } catch (err) {
+        console.log(err)
+      }
+    }
+    compareUser()
+    setCanFollow(userId !== currentUser)  
+  },[currentUser])
+
+  async function handleFollow(){
+    try {
+      const user = await showUser(currentUser)
+      const userData = user.data
+      const userToEdit = { ...userData } 
+      // console.log(inputs)
+      userToEdit.myFollowing.push(userId)
+      const editInput = userToEdit.myFollowing
+      const editBody = {
+        _id: currentUser,
+        myFollows: editInput,  
+      }
+      const response = await editUser(editBody)
+      setFollowing(true)
+      console.log(response)
+      
+    } catch (err) {
+      console.log(err)
+    }
+  }
+
+  async function handleUnFollow(){
+    console.log('unfollow')
+    try { 
+      const user = await showUser(currentUser)
+      const userData = user.data
+      const userToEdit = { ...userData } 
+      const filteredArray = userToEdit.myFollowing.filter(follow => {
+        return follow !== userId
+      })
+      const editBody = {
+        _id: currentUser,
+        myFollows: filteredArray,  
+      }
+      const response = await editUser(editBody)
+      console.log(response)
+      setFollowing(false)
+    } catch (err) {
+      console.log(err)
+    }
+  }
+
+
+
+
+
   return (
     <>
       <section className='userShow'>
@@ -84,6 +162,19 @@ function UserShow() {
           }
           <div>
             {owner && <div><Link to={`/users/${userId}/edit`}>Edit Profile</Link></div>}
+            {canFollow &&
+              <>
+                {!following ? (
+                  <button className='button-outline follow' onClick={handleFollow}>
+                    follow
+                  </button>
+                ) : (
+                  <button className='button-outline follow' onClick={handleUnFollow}>
+                    unfollow
+                  </button>
+                )}
+              </>
+            }
           </div>
         </div>
       </section>  

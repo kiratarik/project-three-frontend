1d1d6e2ac5c98391d1b08fd46680d07b0765b6d4
Tarik Canessa-Wright <tarik.canessa.wright@gmail.com>
2021-10-19 15:30:36 +0100

    unknown

--- src/functionLib/api.js
+++ src/functionLib/api.js
@@ -2,7 +2,6 @@ import axios from 'axios'
 import { getToken } from './auth.js'
 
 import { baseUrl } from '../config.js'
-const baseURL = baseUrl
 
 function getHeaders(){
   const token = getToken()
@@ -15,45 +14,45 @@ function getHeaders(){
 
 
 export function getImages() {
-  return axios.get(`${baseURL}/images`)
+  return axios.get(`${baseUrl}/images`)
 }
 
 export function createImage(imageData) {
-  return axios.post(`${baseURL}/images`, imageData, getHeaders())
+  return axios.post(`${baseUrl}/images`, imageData, getHeaders())
 }
 
 export function getImage(imageId) {
-  return axios.get(`${baseURL}/images/${imageId}`)  
+  return axios.get(`${baseUrl}/images/${imageId}`)  
 }
 
 export function editImage(imageId, imageData) {
-  return axios.put(`${baseURL}/images/${imageId}`, imageData, getHeaders())
+  return axios.put(`${baseUrl}/images/${imageId}`, imageData, getHeaders())
 }
 
 export function rateImage(imageId, formData){
-  return axios.post(`${baseURL}/images/${imageId}/rating`, formData)
+  return axios.post(`${baseUrl}/images/${imageId}/rating`, formData)
 }
 
 export function deleteRating(imageId, ratingId){
-  return axios.get(`${baseURL}/images/${imageId}/rating/${ratingId}`)
+  return axios.get(`${baseUrl}/images/${imageId}/rating/${ratingId}`)
 }
 
 // user requests
 
 export function createUser(formData){
-  return axios.post(`${baseURL}/signUp`, formData)
+  return axios.post(`${baseUrl}/signUp`, formData)
 }
 
 export function logInUser(formData){
-  return axios.post(`${baseURL}/signIn`, formData)
+  return axios.post(`${baseUrl}/signIn`, formData)
 }
 
 export function editUser(userData) {
-  return axios.put(`${baseURL}/users/${userData._id}`, userData, getHeaders())
+  return axios.put(`${baseUrl}/users/${userData._id}`, userData, getHeaders())
 }
 
 export function showUser(userId){
-  return axios.get(`${baseURL}/users/${userId}`)
+  return axios.get(`${baseUrl}/users/${userId}`)
 }
 
 
